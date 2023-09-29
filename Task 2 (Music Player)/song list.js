c5a8f685ccaf9bcf7d504da94c4b2f502e3531df
somu0291 <137806568+somu0291@users.noreply.github.com>
2023-09-29 20:17:03 +0530

    Add files via upload

---
+++ Task 2 (Music Player)/song list.js
@@ -0,0 +1,73 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Once Upon a Time",
+    artist: "Anirudh",
+    img: "image-1.jpeg",
+    src: "music-1.mp3"
+  },
+  {
+    name: "Porkanda Singam",
+    artist: "Anirudh",
+    img: "image-2",
+    src: "music-2"
+  },
+  {
+    name: "Vikram title track",
+    artist: "Anirudh",
+    img: "image-3",
+    src: "music-3"
+  },
+  {
+    name: "vikram track",
+    artist: "Kamalhasan",
+    img: "image-4",
+    src: "music-4"
+  },
+  {
+    name: "Wasted",
+    artist: "Anirudh",
+    img: "image-5",
+    src: "music-5"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
+
+   
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+;
